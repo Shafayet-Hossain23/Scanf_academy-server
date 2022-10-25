409e6a1fdd9527360e16f423272cb7aba9bfb3bd
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-10-25 20:55:06 +0600

    coueses-name

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express()
 const port = process.env.port || 5000
 var cors = require('cors')
 
-
+const courseName = require('./data/courses.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!!')
+})
+app.get('/course-name', (req, res) => {
+    res.send(courseName)
 })
 
 app.listen(port, () => {
