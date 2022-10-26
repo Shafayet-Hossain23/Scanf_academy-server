35d5484d51e514aafcb3f02abbee58fcb5d10e5e
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-10-26 13:02:45 +0600

    course-details-all

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const port = process.env.port || 5000
 var cors = require('cors')
 
 const courseName = require('./data/courses.json')
+const courseDetails = require('./data/course-details.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!')
+    res.send(courseDetails)
 })
 app.get('/course-name', (req, res) => {
     res.send(courseName)
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
